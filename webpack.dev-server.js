6f3dc7c85bf0c0916ffad90e1a725a17ad5ac029
Frank Schüler <SchuelerFrank@gmx.de>
2019-05-06 17:59:29 +0200

    update

--- webpack.dev-server.js
+++ webpack.dev-server.js
@@ -13,10 +13,11 @@ module.exports = merge(common, {
     devServer : {
         contentBase : './WebContent',
         hot : true,
-        quiet : true
+        quiet : true,
+        port : 80
     },
     output : {
-	    filename : 'dev.bundle.js',
+	    filename : project.name + '.js',
         path : path.resolve(__dirname, 'dist')
     },
     plugins : [ new webpack.HotModuleReplacementPlugin() ]  
