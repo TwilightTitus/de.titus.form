8be9ae5c989ca9d3991ff16a5c9be2768f941bfa
Frank Schüler <SchuelerFrank@gmx.de>
2019-06-01 22:51:47 +0200

    update

---
+++ src/Constants.js
@@ -0,0 +1,78 @@
+const Constants = {
+	TYPES : {
+		PAGE : "page",
+		SUMMARY_PAGE : "summary-page",
+		SUBMITTED_PAGE : "submitted-page"
+	},
+	EVENTS : {
+		INITIALIZED : "form-initialized",
+		SUCCESSED : "form-successed",
+		FAILED : "form-failed",
+		STATE_CHANGED : "form-state-changed",
+
+		ACTION_RESET : "form-action-reset",
+		ACTION_SUBMIT : "form-action-submit",
+		ACTION_PAGE_BACK : "form-action-page-back",
+		ACTION_PAGE_NEXT : "form-action-page-next",
+		ACTION_SUMMARY : "form-action-page-summary",
+		ACTION_LIST_FIELD_ADD : "form-action-list-field-add",
+		ACTION_LIST_FIELD_REMOVE : "form-action-list-field-remove",
+
+		PAGE_INITIALIZED : "form-page-initialized",
+		PAGE_CHANGED : "form-page-changed",
+		PAGE_SHOW : "form-page-show",
+		PAGE_HIDE : "form-page-hide",
+		PAGE_SUMMARY : "form-page-summary",
+		PAGE_SUBMITTED : "form-page-submitted",
+
+		FIELD_VALIDATED : "form-field-validated",
+		FIELD_SHOW : "form-field-show",
+		FIELD_HIDE : "form-field-hide",
+		FIELD_SUMMARY : "form-field-SUMMARY",
+		FIELD_VALUE_CHANGED : "form-field-value-changed",
+
+		VALIDATION_STATE_CHANGED : "form-validation-state-changed",
+		VALIDATION_VALID : "form-validation-valid",
+		VALIDATION_INVALID : "form-validation-invalid",
+
+		CONDITION_STATE_CHANGED : "form-condition-state-changed",
+		CONDITION_MET : "form-condition-met",
+		CONDITION_NOT_MET : "form-condition-not-met",
+
+		BUTTON_INACTIVE : "form-button-inactive",
+		BUTTON_ACTIVE : "form-button-active",
+	},
+	STATE : {
+		INPUT : "form-state-input",
+		SUBMITTED : "form-state-submitted",
+	},
+	ATTRIBUTE : {
+		VALIDATION : "-validation",
+		VALIDATION_FAIL_ACTION : "-validation-fail-action",
+		CONDITION : "-condition",
+		MESSAGE : "-message"
+	},
+	SPECIALSTEPS : {
+		START : "form-step-start",
+		SUMMARY : "form-step-summary",
+		SUBMITTED : "form-step-submitted"
+	},
+	STRUCTURELEMENTS : {
+		FORM : {
+			selector : "[data-form]"
+		},
+		PAGE : {
+			selector : "[data-form-page]"
+		},
+		SINGLEFIELD : {
+			selector : "[data-form-field]"
+		},
+		CONTAINERFIELD : {
+			selector : "[data-form-container-field]"
+		},
+		LISTFIELD : {
+			selector : "[data-form-list-field]"
+		}
+	}
+};
+export default Constants;
